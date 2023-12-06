44b960bc703ec20183a52f900f847ea4cd723bf6
Jørgen Sneisen <jorgen.sneisen@where2o.com>
2023-12-06 01:20:19 +0100

    Day 5 bugfix

--- src/routes/day/5/+page.js
+++ src/routes/day/5/+page.js
@@ -1,4 +1,5 @@
-export let ssr = false;
+export const ssr = false;
+export const prerender = false;
 export async function load({fetch}) {
 	const events = await fetch("https://advent.sveltesociety.dev/data/2023/day-five.json").then(r => r.json());
 	return {
