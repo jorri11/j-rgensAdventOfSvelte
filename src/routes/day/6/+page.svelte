dcc537042b52506a855e792c9408e5e1fe7e4b84
Jørgen Sneisen <jorgen.sneisen@where2o.com>
2023-12-06 23:35:02 +0100

    Basic day 6

---
+++ src/routes/day/6/+page.svelte
@@ -0,0 +1,145 @@
+<script>
+	let beats = [];
+	let numberOfBeats;
+
+	$: accumulatedDifferenceBetweenBeats =
+		beats.length >= 2
+			? beats.reduce((acc, curr, index) => {
+					if (index > 0) {
+						acc += curr - beats[index - 1];
+					}
+					return acc;
+			  }, 0)
+			: 0;
+	$: avreageBeats = beats.length >= 2 ? accumulatedDifferenceBetweenBeats / (beats.length - 1) : 0;
+
+	$: bpm = beats.length >= 2 ? 60000 / avreageBeats : 0;
+
+	function onBeat(e) {
+		if (beats.length == numberOfBeats) {
+			beats.shift();
+		}
+		beats.push(Date.now());
+		beats = beats;
+	}
+	function onKeyPress(e) {
+		if (e.code == 'Space') {
+			onBeat(e);
+		}
+	}
+	function reset() {
+		beats = [];
+	}
+</script>
+
+<svelte:window on:keypress={onKeyPress} />
+<div class="page-wrapper">
+	<h1>Misteltoe metronome</h1>
+	<section class="the-actual-assignment">
+		<p>Press spacebar to the beat or press <span class="flair">The Big Purple Button™</span></p>
+		<div class="centering">
+			<div class="realtime-wrapper">
+				<div class="realtime-top"></div>
+				<div class="realtime red">
+					<p>{Math.round(bpm) ?? 0} bpm</p>
+				</div>
+			</div>
+		</div>
+		<div class="button-wrapper">
+			<button class="increase" on:click={reset}>Reset</button>
+			<select bind:value={numberOfBeats} class="decrease">
+				<option value={4} selected>Low smooting</option>
+				<option value={8}>Medium smooting</option>
+				<option value={16}>High smooting</option>
+			</select>
+
+			<button on:click={onBeat} class="big-purple-button">The Big Purple Button™</button>
+		</div>
+	</section>
+	<!--	<section>
+		<h2>Reccomendations</h2>
+	</section>-->
+</div>
+
+<style>
+	.increase {
+		background-color: var(--christmas-red);
+		color: var(--christmas-gold);
+		border: var(--christmas-gold) 2px solid;
+	}
+
+	.decrease {
+		background-color: var(--christmas-green);
+		color: var(--christmas-gold);
+		border: var(--christmas-gold) 2px solid;
+	}
+	.centering {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		padding-bottom: 1rem;
+	}
+	.page-wrapper {
+		display: grid;
+		grid-template-columns: 1fr 1fr;
+		grid-gap: 1rem;
+		padding: 0 0.5rem 0.5rem 0.5rem;
+		& h1 {
+			grid-column: 1 / -1;
+			text-align: center;
+		}
+		& .the-actual-assignment {
+			grid-column: 1 / -1;
+		}
+	}
+	.button-wrapper {
+		display: grid;
+		grid-template-columns: 1fr 1fr;
+		gap: 1rem;
+	}
+	.big-purple-button {
+		grid-column: 1 / -1;
+		background-color: var(--christmas-purple);
+		color: var(--christmas-gold);
+		padding: 10px;
+		border-radius: 10px;
+		border: none;
+		font-size: 1.5rem;
+	}
+	.realtime-wrapper {
+		position: relative;
+		width: min-content;
+		margin-top: 50px;
+	}
+	.realtime {
+		width: 200px;
+		height: 200px;
+		border-radius: 50%;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		font-size: 2rem;
+	}
+	.realtime.red {
+		background-color: var(--christmas-red);
+	}
+	.realtime-top {
+		position: absolute;
+		top: -35px;
+		z-index: -1;
+		left: 50%;
+		transform: translateX(-50%);
+		background-color: var(--christmas-gold);
+		width: 30px;
+		height: 40px;
+	}
+	.flair {
+		animation: someanimation 1s infinite;
+		color: var(--christmas-gold);
+	}
+	@keyframes someanimation {
+		50% {
+			color: var(--christmas-red);
+		}
+	}
+</style>
